ea4de922726717beecb427ec312ab7e5c58a36fc
Kadir Egilmez <kegilmez@kadirs-mbp.home>
2020-02-05 00:10:25 +0000

    Updates

---
+++ precache-manifest.22b0c5e86b3ccd9a264aed77a3d985c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb2b017f29747794e6755a35f5ff2ed",
+    "url": "/laperla11/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "94066e755e7c5c1ebaf9",
+    "url": "/laperla11/monsters-rolodex/static/css/main.1647281c.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/laperla11/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/laperla11/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94066e755e7c5c1ebaf9",
+    "url": "/laperla11/monsters-rolodex/static/js/main.1f2ef317.chunk.js"
+  },
+  {
+    "revision": "68e1ce8d9c59c31110ef",
+    "url": "/laperla11/monsters-rolodex/static/js/runtime-main.fb50fb13.js"
+  }
+]);
